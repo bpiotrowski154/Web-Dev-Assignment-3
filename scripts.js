8ce52f8f69dc644fa6ace2370cc3287a30e4985d
bpiotrowski154 <88128424+bpiotrowski154@users.noreply.github.com>
2023-03-18 19:14:03 -0500

    Updated JQuery/JS file

--- scripts.js
+++ scripts.js
@@ -1,48 +1,152 @@
-function validate() {
-    form.preventDefault();
-    let validation = true;
-    let username = document.forms["theForm"]["username"].value;
-    let email = document.forms["theForm"]["email"].value;
+$(document).ready(function(){
+    $(".req").hide();
+    $(".pass").hide();
+    $(".the-form").width("300px");
+    
+    $('#submit').click(function (e) {
+        e.preventDefault();
 
-    var currChar;
-    if(username.length == 0 || username.length < 6 || username.length >10){
-        document.getElementById("theForm").style.width = "600px";
-        document.getElementById("username").style.borderColor = "red";
-        //document.getElementById("username_error").innerHTML = "Username must have 6-10 characters (a-z, A-Z, _)"
-        return false;
-    }
-    else {
-        for(let i = 0; i < username.length; i++){
-            currChar = username.charCodeAt(i);
-            if(!(code > 65 && code < 91) /*A-Z*/ && !(code > 96 && code < 123) /*a-z*/ && !(code ==95) /*_*/) {
-                document.getElementById("theForm").style.width = "600px";
-                document.getElementById("username").style.borderColor = "red";
-                return false; 
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const position = document.getElementById("position").value;
+        const fulltime = document.getElementById("parttime").checked;
+        const parttime = document.getElementById("fulltime").checked;
+
+        if (username == "" || username.length < 6 || username.length > 10) { //Check username for length
+            $("#userpass").hide();
+            $("#userfail").show();
+            $("#username").addClass("error");
+            $(".the-form").width("500px");
+        }
+        else {
+            let currChar, charPass = true;
+            for(let i = 0; i < username.length; i++){ //Check for valid characters
+                currChar = username.charCodeAt(i);
+                if(!(currChar > 64 && currChar < 91) /*A-Z*/ && !(currChar > 96 && currChar < 123) /*a-z*/ && !(currChar ==95) /*_*/) {
+                    charPass =false;
+                    break;
+                }
+            }
+            if(charPass){ //pass
+                $("#userfail").hide();
+                $("#username").removeClass("error");
+                $("#userpass").show();                
+            }
+            else {
+                $("#userpass").hide();
+                $("#userfail").show();
+                $("#username").addClass("error");
+                $(".the-form").width("500px");
             }
         }
-        return true;
-    }
 
-    //validation = usernameValid(username);
-
-}
-
-function usernameValid(username){
-    var currChar;
-    if(username.length == 0 || username.length < 6 || username.length >10){
-        document.getElementById("theForm").style.width = "500px";
-        document.getElementById("username").style.borderColor = "red";
-        return false;
-    }
-    else {
-        for(let i = 0; i < username.length; i++){
-            currChar = username.charCodeAt(i);
-            if(!(code > 65 && code < 91) /*A-Z*/ && !(code > 96 && code < 123) /*a-z*/ && !(code ==95) /*_*/) {
-                document.getElementById("theForm").style.width = "500px";
-                document.getElementById("username").style.borderColor = "red";
-                return false; 
+        if (email.length < 5) { //impossible to be valid email
+            $("#emailpass").hide();
+            $("#emailfail").show();
+            $("#email").addClass("error");
+            if($(".the-form").width() != "500px"){
+                $(".the-form").width("500px");
             }
         }
-        return true;
-    }
-}
+        else {
+            let currChar, charPass = true, temp = 0;
+            //y_1@y_2.y_3
+            for(let i = 0; i < email.length; i++){ //Ensuring y_1 is alphanumerical or _
+                currChar = email.charCodeAt(i);
+                if(i == 0 && (currChar == 64 || currChar == 46)){
+                    charPass = false;
+                    break;
+                }
+                else if (currChar == 64 && i == email.length-1){
+                    charPass = false;
+                    break;
+                }
+                else if (currChar == 64){
+                    temp = i+1;
+                    break;
+                }
+                else if(!(currChar > 64 && currChar < 91) /*A-Z*/ && !(currChar > 96 && currChar < 123) /*a-z*/ && !(currChar ==95) /*_*/ && !(currChar > 47 && currChar < 58)){
+                    charPass = false;
+                    break;
+                }
+            }
+            for(let i = temp; i < email.length; i++){ //Ensuring y_2 is alphanumerical or _
+                currChar = email.charCodeAt(i);
+                if((i == temp && currChar == 46) || currChar == 64){
+                    charPass = false;
+                    break;
+                }
+                else if (currChar != 46 && i == email.length-1){
+                    charPass = false;
+                    break;
+                }
+                else if (currChar == 46){
+                    temp = i+1;
+                    break;
+                }
+                else if (!(currChar > 64 && currChar < 91) /*A-Z*/ && !(currChar > 96 && currChar < 123) /*a-z*/ && !(currChar ==95) /*_*/ && !(currChar > 47 && currChar < 58)){
+                    charPass = false;
+                    break;
+                }
+            }
+            for (let i = temp; i < email.length; i++){ //Ensuring y_3 is alphanumerical or _
+                currChar = email.charCodeAt(i);
+                if(currChar == 46 || currChar == 64){
+                    charPass = false;
+                    break;
+                }
+                else if(!(currChar > 64 && currChar < 91) /*A-Z*/ && !(currChar > 96 && currChar < 123) /*a-z*/ && !(currChar ==95) /*_*/ && !(currChar > 47 && currChar < 58)){
+                    charPass = false
+                    break;
+                }
+            }
+
+            if(charPass){
+                $("#emailfail").hide();
+                $("#email").removeClass("error");
+                $("#emailpass").show();
+            }
+            else{
+                $("#emailpass").hide();
+                $("#emailfail").show();
+                $("#email").addClass("error");
+                if($(".the-form").width() != "500px"){
+                    $(".the-form").width("500px");
+                }
+            }    
+        }
+
+        if (!position) {
+            $("#positionpass").hide();
+            $("#positionfail").show();
+            $("#position").addClass("error");
+            if($(".the-form").width() != "500px"){
+                $(".the-form").width("500px");
+            }
+        }
+        else {
+            $("#positionfail").hide();
+            $("#position").removeClass("error");
+            $("#positionpass").show();
+        }
+
+        if (!fulltime && !parttime) {
+            $("#fulltimepass").hide();
+            $("#fulltimefail").show();
+            if($(".the-form").width() != "500px"){
+                $(".the-form").width("500px");
+            }
+        }
+        else {
+            $("#fulltimefail").hide();
+            $("#fulltimepass").show();
+        }
+
+        if($("#userpass").is(":visible") && $("#emailpass").is(":visible") && $("#positionpass").is(":visible") && $("#fulltimepass").is(":visible") && $(".the-form").width() != "300px"){
+            $(".the-form").width("300px");
+        }
+
+        $("#test").html(fulltime.value);
+    })
+    
+});
